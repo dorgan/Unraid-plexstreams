033e19942aeff57c6ae2b4e76778f6674049c90e
Donald Organ <dorgan@donaldorgan.com>
2020-06-09 15:16:25 -0400

    2020.06.09a

--- src/plexstreams/usr/local/emhttp/plugins/plexstreams/ajax.php
+++ src/plexstreams/usr/local/emhttp/plugins/plexstreams/ajax.php
@@ -1,20 +1,27 @@
 <?php
     include('/usr/local/emhttp/plugins/plexstreams/includes/config.php');
     include('/usr/local/emhttp/plugins/plexstreams/includes/common.php');
+
+    header('Content-type: application/json');
+
     $mergedStreams = [];
 
     if (!empty($cfg['TOKEN'])) {
-        $host =  (substr($cfg['HOST'], -1) !== '/' ? $cfg['HOST'] : substr($cfg['HOST'],0,-1));
-        if (isset($_REQUEST['host'])) {
-            $host = $_REQUEST['host'];
+        if ($cfg['HOST'] !== '') {
+            $host =  (substr($cfg['HOST'], -1) !== '/' ? $cfg['HOST'] : substr($cfg['HOST'],0,-1));
+            if (isset($_REQUEST['host'])) {
+                $host = $_REQUEST['host'];
+            }
+
+            $streams = getStreams($host, $cfg);
+            
+            $mergedStreams = mergeStreams($streams);
+            if (isset($_REQUEST['dbg'])) {
+                v_d($mergedStreams);
+            }
+            echo(json_encode($mergedStreams));
+        } else {
+            http_response_code(500);
         }
 
-        $streams = getStreams($host, $cfg);
-        
-        $mergedStreams = mergeStreams($streams);
-        if (isset($_REQUEST['dbg'])) {
-            v_d($mergedStreams);
-        }
-        header('Content-type: application/json');
-        echo(json_encode($mergedStreams));
     }
